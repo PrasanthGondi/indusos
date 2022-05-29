18775cd6c65e7c80e5de6c936fee6c47004154c9
PrasanthGondi <gprasanthchowdary@gmail.com>
2022-05-29 05:50:00 +0000

    Adde

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 const dbpath = path.join(__dirname, "app.db");
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3004" }));
 //app.use(bodyParser.urlencoded({ extended: true }));
 let db = null;
 
